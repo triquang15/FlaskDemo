4a802cb5f276905d599f94c8b578cad5f8578a6b
Tri Quang <triquang.15qt@gmail.com>
2025-02-23 11:04:36 +0700

    Init modal user

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from extension import db
 
 app = Flask(__name__)
+db.init_app(app)
 
 @app.route('/')
 def home():
---
+++ models/user.py
@@ -0,0 +1,31 @@
+from extension import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False, unique=True)
+    contact = db.Column(db.String(50), unique=True, nullable=False, unique=True)
+    image = db.Column(db.String(255), nullable=True)
+    password = db.Column(db.Text, nullable=False)
+    biography = db.Column(db.Text, nullable=True)
+    user_type = db.Column(db.String(20), default='author')
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now)
+    
+    def __init__(self, first_name, last_name, email, contact, password, biography, user_type, image=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.contact = contact
+        self.password = password
+        self.biography = biography
+        self.user_type = user_type
+        
+        
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+        
+    
